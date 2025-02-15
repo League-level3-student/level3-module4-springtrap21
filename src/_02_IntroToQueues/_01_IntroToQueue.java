728e8d090f5146bc99c89673e3be2a8ef519ff92
drake <drake@GlizzyGobbler>
2025-02-15 11:27:30 -0800

    introdone

--- src/_02_IntroToQueues/_01_IntroToQueue.java
+++ src/_02_IntroToQueues/_01_IntroToQueue.java
@@ -1,4 +1,9 @@
 package _02_IntroToQueues;
+
+import java.util.ArrayDeque;
+import java.util.Random;
+import java.util.Stack;
+
 /* 
  * OBJECTIVE:
  * 1. Push 100 double values onto a Stack.
@@ -34,24 +39,42 @@ public class _01_IntroToQueue {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles using the Stack class
         //    Note: you have to use the capitalized Double and not double
-
+    	Stack<Double> dubs = new Stack<>();
         // 2. Use a loop to add 100 random doubles between 0 and 100 to the Stack
-
+    	Random rnd = new Random();
+    	for (int i = 0; i < 100; i++) {
+			dubs.push(rnd.nextDouble()*100);
+		}
         // 3. Create a Queue of Doubles using the ArrayDeque class
         //    Note: you have to use the capitalized Double and not double
-
+    	ArrayDeque<Double> que = new ArrayDeque<>();
         // 4. Pop off 5 elements from the Stack and add them to the Queue 
-
+    	for (int i = 0; i < 5; i++) {
+			que.add(dubs.pop());
+		}
         // 5. Print and remove a random number of elements, from 1 to 5 elements,
         //    from the front of the Queue. Example:
         //    "removing 3 elements from Queue: 25 57 2"
-
+    	int remove = rnd.nextInt(5)+1;
+    	System.out.println("Removing " + remove + " numbers from the Queue");
+    	for (int i = 0; i < remove; i++) {
+			que.pop();
+		}
         // 6. Pop off as many elements from the stack to fill the Queue with 5
         //    elements. If there aren't enough elements in the Stack to fill the 
         //    queue, fill the queue as much as possible. 
-
+    	for (int i = 0; i < remove; i++) {
+    		que.add(dubs.pop());
+		}
         // 7. Loop until there are no more elements in either the Stack or Queue
         //    and all the elements are printed
-        
+        while (!dubs.empty()) {
+			System.out.println(dubs.pop());
+		}
+        System.out.println("Stack contains " + dubs.size() + " items.");
+        while (!que.isEmpty()) {
+			System.out.println(que.pop());
+		}
+        System.out.println("Queue contains " + que.size() + " items.");
     }
 }
